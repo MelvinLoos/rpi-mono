c4703b7d01aef0be10f03c90f0f9dd381d1b9be0
Melvin Loos <melvin@melvinloos.nl>
2017-02-14 23:02:05 +0100

    Added extra command to /dev/null

--- build.sh
+++ build.sh
@@ -14,7 +14,7 @@ wget https://github.com/mono/mono/archive/mono-4.8.0.478.tar.gz
 tar -zxvf mono-4.8.0.478.tar.gz
 rm mono-4.8.0.478.tar.gz
 cd mono-mono-4.8.0.478
-./autogen.sh --prefix=$PREFIX
+./autogen.sh --prefix=$PREFIX 2>&1 >/dev/null
 echo "Starting make..."
 make 2>&1 >/dev/null
 echo "Starting make install..."
